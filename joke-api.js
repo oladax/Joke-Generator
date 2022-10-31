e12b2058492499773b05e15deeadfc0bb9afbd76
oladax <106500491+oladax@users.noreply.github.com>
2022-10-31 05:20:25 -0500

    Update joke-api.js

--- joke-api.js
+++ joke-api.js
@@ -18,7 +18,18 @@ function acquireJoke() {
         
         
 const jokespeech = (jokeDiv.innerHTML)
- responsiveVoice.speak(jokespeech)
+var speech = new SpeechSynthesisUtterance();
+
+    // defing speech properties
+    speech.lang =  "en-NG";
+    speech.text = jokespeech;
+    speech.volume = 30;
+    speech.pitch = 1;
+    speech.rate = 1;
+
+
+    window.speechSynthesis.speak(speech)
+
       });
     
   }
